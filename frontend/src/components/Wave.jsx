3e5b941367a616db70295871a078d1d57587b7ff
sebastien <romeo.sebastien.dev@gmail.com>
2023-12-30 15:40:49 +0100

    wave fait pour desktop

---
+++ frontend/src/components/Wave.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import wave from "../assets/vague1.png";
+import boat from "../assets/boat2.png";
+import "./Wave.css";
+
+function Wave() {
+  return (
+    <div className="container-wave">
+      <img src={wave} alt="vague" className="wave" />
+      <img src={boat} alt="boat" className="boat" />
+    </div>
+  );
+}
+
+export default Wave;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-
-import "./Home.css";
 import Navbar from "../components/Navbar";
 import Carousel from "../components/Carousel";
+
+import "./Home.css";
+import Wave from "../components/Wave";
 
 function Home() {
   return (
     <div className="all-page">
       <Navbar />
+
       <Carousel />
+
+      <Wave />
     </div>
   );
 }
