7b563272953d00f2001cd2d675fc95e1f4fd2b2c
sebastien <romeo.sebastien.dev@gmail.com>
2024-01-31 12:24:14 +0100

    debut du carrousel fait pour comment

--- frontend/src/components/CommentAdd.jsx
+++ frontend/src/components/CommentAdd.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import StarRatings from "react-star-ratings";
 import { useLoaderData } from "react-router-dom";
 import "./CommentAdd.css";
 
 function CommentAdd() {
   const allComments = useLoaderData();
+  const [currentCommentIndex, setCurrentCommentIndex] = useState(0);
 
   const calculateAverageRating = () => {
     if (allComments.length === 0) {
@@ -20,6 +21,18 @@ function CommentAdd() {
 
   const averageRating = calculateAverageRating();
 
+  const handleNextComment = () => {
+    setCurrentCommentIndex((prevIndex) =>
+      prevIndex === allComments.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handlePrevComment = () => {
+    setCurrentCommentIndex((prevIndex) =>
+      prevIndex === 0 ? allComments.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div>
       <h1 className="comment-avis">
@@ -33,21 +46,33 @@ function CommentAdd() {
         />
       </h1>
       <div className="bloc-avis">
-        {allComments.map((comment) => (
-          <div key={comment.id} className="comment-container">
+        {" "}
+        <button type="button" onClick={handlePrevComment}>
+          Previous
+        </button>
+        {allComments.length > 0 && (
+          <div
+            key={allComments[currentCommentIndex].id}
+            className="comment-container"
+          >
             <h2>
-              {comment.name} a donné :{" "}
+              {allComments[currentCommentIndex].name} a donné :{" "}
               <StarRatings
-                rating={comment.rating}
+                rating={allComments[currentCommentIndex].rating}
                 starRatedColor="gold"
                 numberOfStars={5}
                 name="rating"
               />
             </h2>
             <h2> Son commentaire : </h2>
-            <h3>{comment.avis}</h3>
+            <h3>{allComments[currentCommentIndex].avis}</h3>
           </div>
-        ))}
+        )}
+        {allComments.length > 1 && (
+          <button type="button" onClick={handleNextComment}>
+            Next
+          </button>
+        )}
       </div>
     </div>
   );
