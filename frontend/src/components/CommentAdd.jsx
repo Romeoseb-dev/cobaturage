bea09250f3d5f7f0f6bc7e01c6bb09e505c1e63b
sebastien <romeo.sebastien.dev@gmail.com>
2024-01-31 11:35:35 +0100

    affichage des com

--- frontend/src/components/CommentAdd.jsx
+++ frontend/src/components/CommentAdd.jsx
@@ -1,25 +1,54 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
 import StarRatings from "react-star-ratings";
+import { useLoaderData } from "react-router-dom";
+import "./CommentAdd.css";
 
 function CommentAdd() {
   const allComments = useLoaderData();
 
+  const calculateAverageRating = () => {
+    if (allComments.length === 0) {
+      return 0;
+    }
+
+    const totalRating = allComments.reduce(
+      (acc, comment) => acc + comment.rating,
+      0
+    );
+    return totalRating / allComments.length;
+  };
+
+  const averageRating = calculateAverageRating();
+
   return (
     <div>
-      <h2>Avis</h2>
-      {allComments.map((comment) => (
-        <div key={comment.id}>
-          <StarRatings
-            rating={comment.rating}
-            starRatedColor="gold"
-            numberOfStars={5}
-            name="rating"
-          />
-          <h3>{comment.name}</h3>
-          <h3>{comment.avis}</h3>
-        </div>
-      ))}
+      <h1 className="comment-avis">
+        Avis{" "}
+        <StarRatings
+          rating={averageRating}
+          starRatedColor="gold"
+          numberOfStars={5}
+          name="averageRating"
+          readOnly
+        />
+      </h1>
+      <div className="bloc-avis">
+        {allComments.map((comment) => (
+          <div key={comment.id} className="comment-container">
+            <h2>
+              {comment.name} a donné :{" "}
+              <StarRatings
+                rating={comment.rating}
+                starRatedColor="gold"
+                numberOfStars={5}
+                name="rating"
+              />
+            </h2>
+            <h2> Son commentaire : </h2>
+            <h3>{comment.avis}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
